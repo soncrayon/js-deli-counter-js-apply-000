cbdb5e0027ad61934fbcf9c0cb9f1fa15f099f07
Curtis Arnold <arnold.curtis@gmail.com>
2019-06-09 20:40:40 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,4 @@
+// it('adds a person to the line', () => {
+      //expect(takeANumber(katzDeli, 'Ada')).toEqual("Welcome, //Ada. You are number 1 in line.");
+     // expect(katzDeli).toEqual(['Ada']);
+   // });
