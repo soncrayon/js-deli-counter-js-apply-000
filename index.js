2dd617e663a249d64bffd7b720d55c374423437c
Curtis Arnold <arnold.curtis@gmail.com>
2019-06-09 22:10:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ function currentLine (katzDeli) {
   if (katzDeli.length === 0) {
     return "The line is currently empty.";
   } else{
-  let line = "";
-  for (let i = 0; i < katzDeli.length; i++) {
-    line = line + `${i + 1}. ${katzDeli[i]}, `; 
+  let line = `1. ${katzDeli[0]}`;
+  for (let i = 1; i < katzDeli.length; i++) {
+    line = line + `, ${i + 1}. ${katzDeli[i]}`; 
   }
   return `The line is currently: ${line}`; 
 }
