196f312281f4a866b9c3e58252d034ff40858360
Curtis Arnold <arnold.curtis@gmail.com>
2019-06-09 21:07:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ function takeANumber (katzDeli, name) {
 }
 
 function currentLine (katzDeli) {
+  if (katzDeli == []) {
+    return "The line is currently empty."
+  }
   let line = "";
   for (let i = 0; i < katzDeli.length; i++) {
     line.push(((indexOf(i)) + 1) + `.${i},`); 
