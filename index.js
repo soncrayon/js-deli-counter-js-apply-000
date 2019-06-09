a6fac327fd9e2c81f655c1d5ad34a80ba3643828
Curtis Arnold <arnold.curtis@gmail.com>
2019-06-09 20:49:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
      // expect(katzDeli).toEqual(['Ada']);
    // });
 
+let katzDeli = []; 
+
 function takeANumber (katzDeli, name) {
   katzDeli.push(name); 
   return `Welcome, ${name}. You are number ${katzDeli.indexOf(name) + 1} in line.`;
